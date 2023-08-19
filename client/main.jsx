7d26033c3ae50ff3a1812c918b40b19cb0e3d878
PedroPini <augustopini@gmail.com>
2023-08-19 12:08:04 +0800

    Adding collection

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
-
+import { TasksCollection } from "../api/TasksCollection"
+const insertTask = taskText => TasksCollection.insert({ text: taskText })
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
